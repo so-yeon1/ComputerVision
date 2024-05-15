48ef5c7e9a357fbb9a4508ef75c64af7418f0bc2
so-yeon1 <psy92411@gmail.com>
2024-05-16 04:58:53 +0900

    메모리 크기 함수 수정

--- 8.4/knn_8.4.py
+++ 8.4/knn_8.4.py
@@ -3,35 +3,46 @@ import numpy as np
 import os
 import sys
 from pympler import asizeof
+from memory_profiler import profile
+import psutil
 
-def save_model(model, filename):
+def model_size(model, filename):
     model.save(filename)
-    return os.path.getsize(filename), asizeof.asizeof(filename)
+    pid = os.getpid()
+    process = psutil.Process(pid)
+    memory = process.memory_info().rss
+    memory_size = memory
+    # print(f"사용 중인 메모리: {memory / 1024 ** 2}MiB")
 
-def create_knn_model():
-    knn = cv2.ml.KNearest_create()
-    initial_size, initial_mem = save_model(knn, 'knn_initial.xml')
-    print(f"Initial KNN model size: {initial_size} bytes")
-    print(f"Initial KNN model memory: {initial_mem} bytes")
+    return os.path.getsize(filename), asizeof.asizeof(model) # sys.getsizeof(model) #memory_size
+
+def initial_knn(knn):
+    initial_size, initial_mem = model_size(knn, 'knn_initial.xml')
+    print("[학습 전]")
+    print(f"knn 크기: {initial_size} bytes")
+    print(f"knn 메모리: {initial_mem} bytes")
     print()
     return knn
 
-def train_knn_model(knn, features, labels):
-    knn.train(features, cv2.ml.ROW_SAMPLE, labels)
-    trained_size, trained_mem = save_model(knn, 'knn_trained.xml')
-    print(f"Trained KNN model size: {trained_size} bytes")
-    print(f"Trained KNN model memory: {trained_mem} bytes")
+def trained_knn(knn):
+    trained_size, trained_mem = model_size(knn, 'knn_trained.xml')
+    print("[학습 후]")
+    print(f"knn 크기: {trained_size} bytes")
+    print(f"knn 메모리: {trained_mem} bytes")
 
+@profile
 def main():
-    L=20000
+    L=20
     data = np.random.randint(0, 100, (L, 2)).astype(np.float32)
     labels = np.random.randint(0, 2, (L, 1)).astype(np.int32)
 
     # KNN 모델 생성 및 크기 측정
-    knn = create_knn_model()
+    knn = cv2.ml.KNearest_create()
+    knn = initial_knn(knn)
 
     # KNN 모델 학습 및 크기 측정
-    train_knn_model(knn, data, labels)
+    knn.train(data, cv2.ml.ROW_SAMPLE, labels)
+    trained_knn(knn)
 
 if __name__ == "__main__":
-    main()
+	main()
