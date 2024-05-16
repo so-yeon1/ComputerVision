1c7daadd45c3906a768978c794ac2a3d41e2895d
so-yeon1 <psy92411@gmail.com>
2024-05-16 23:10:28 +0900

    주석제거

--- 9.4/knn_9.4.py
+++ 9.4/knn_9.4.py
@@ -21,9 +21,8 @@ import matplotlib.pyplot as plt
 # 시드 값 설정
 np.random.seed(4222)
 
-image_size = (200, 200, 3)  ############ 수정?
-image = np.zeros(image_size, dtype=np.uint8)    ############ 수정? (변수만 넣는게 더 좋을 것 같음.)
-
+image_size = (200, 200, 3)
+image = np.zeros(image_size, dtype=np.uint8)
 # 학습 데이터 생성
 L = 20
 data = np.random.randint(0, 200, (L, 2)).astype(np.float32)
@@ -33,8 +32,8 @@ class_distribution = [0.5, 0.5]  # 클래스 0과 클래스 1의 비율
 labels = np.random.choice([0, 1], size=(L, 1), p=class_distribution).astype(np.float32) # 0,1 중에 무작위 추출. 0: 0.5, 1: 0.5확률로 추출
 
 rand_test_data = np.random.randint(0, 200, (5, 2)).astype(np.float32)
-# test_labels = np.random.choice([0, 1], size=(L, 1), p=class_distribution).astype(np.float32)    ########### 수정(test데이터의 레이블은 필요없음)
 
+# test_data(2차원 그리드) 생성
 a = np.linspace(0, 199, 200)
 b = np.linspace(0, 199, 200)
 x, y = np.meshgrid(a, b)  # 2차원 그리드 생성
@@ -67,20 +66,16 @@ def draw_image(k):
 
 
     # k 값에 따라 이웃을 고려하여 원 그리기
-    for idx, point in enumerate(rand_test_data):  ############ 수정 (label은 필요x)
+    # 1)neighbors가 동점이 나왔을 경우 AND 2) 만약 result와 neighbors의 1위가 같지 않을 경우 원 그리기  3) nearest함수 쓰지 말고 neighbor[인덱스]로 직접접근
+    for idx, point in enumerate(rand_test_data):
         label_text = idx  # 인덱스를 사용하여 0부터 오름차순으로 레이블 값 설정
-        # if label == # 1)neighbors가 동점이 나왔을 경우:o  2) 만약 label과 neighbors의 1위가 같지 않으면 원 그리기:o  3) nearest함수 쓰지 말고 neighbor[인덱스]로 직접접근
 
-        # plt.text(point[0], point[1], str(label_text), fontsize=12, color='black', ha='center', va='center') ######### 수정(중복. 하나는 없어도 됨)
-        ret, results, neighbor, dist = knn.findNearest(point.reshape(1, -1), k=k)  ############## 수정(point.reshape(1,-1)으로 2차원배열으로 변경)
-        print(neighbor[0][0])
-        print(results)
+        ret, results, neighbor, dist = knn.findNearest(point.reshape(1, -1), k=k)
         unique, cnt = np.unique(neighbor, return_counts=True)
         max_cnt = np.max(cnt)
-        print(dist[0])
-        if (cnt == max_cnt).sum() > 1 and (neighbor[0][0] != results):      # 동점인 경우
-            # radius = np.mean(dist[0]) ** 0.5
-            radius = np.sqrt(dist[0, k - 1])  ########### 수정(mean을 쓴 이유가 있는지?)
+
+        if (cnt == max_cnt).sum() > 1 and (neighbor[0][0] != results):      # 동점인 경우 and (neighbor!=result)인 경우
+            radius = np.sqrt(dist[0, k - 1])
             # 빨간색에 가까우면 검정 파란색에 가까우면 흰색
             circle_color = 'b' if results[0][0] == 0 else 'r'
             plt.text(point[0], point[1], str(label_text), fontsize=20, color=circle_color)
@@ -92,7 +87,6 @@ def draw_image(k):
     plt.title(f'KNN Visualization (k={k})')
     plt.legend()
     plt.grid(True)
-    # plt.axis('equal')             ############ 수정
     plt.show()
 
     # 정확도 계산
@@ -100,18 +94,12 @@ def draw_image(k):
     accuracy = np.mean(result == labels)
     print(f'Accuracy: {accuracy:#.2f}'+"%" " " f'k :  {k}')
 
-
-# # 트랙바 콜백 함수       ######### 수정(굳이 없어도 됨. createTrackbar에서 바로 draw_image 호출)
-# def on_trackbar(val):
-#     k = cv2.getTrackbarPos('k', 'KNN Visualization')
-#     draw_image(k)
-
 # 트랙바 생성
 cv2.namedWindow('KNN Visualization')
 cv2.createTrackbar('k', 'KNN Visualization', 1, 5, draw_image)
 
 # 초기 이미지 표시
-draw_image(1)       ############### 수정(위치)
+draw_image(1)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
