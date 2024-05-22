65c5400658a42a85af57ad4ada1238d1d144f96a
so-yeon1 <psy92411@gmail.com>
2024-05-22 21:36:12 +0900

    주석있는버전

--- 9.4/knn_9.4.py
+++ 9.4/knn_9.4.py
@@ -19,19 +19,27 @@ import cv2
 import matplotlib.pyplot as plt
 
 # 시드 값 설정
-np.random.seed(4222)
+np.random.seed(51)
 
 image_size = (200, 200, 3)
 image = np.zeros(image_size, dtype=np.uint8)
 # 학습 데이터 생성
 L = 20
+
 data = np.random.randint(0, 200, (L, 2)).astype(np.float32)
+data = np.unique(data, axis=0)  # 중복데이터 제거
+
+while len(data) < L:       # 지정한 학습데이터 수만큼 중복없는 데이터 생성
+    newData = np.random.randint(0,200,(L-len(data),2)).astype(np.float32)
+    data = np.append(data, newData, axis=0) # 제거된 중복데이터 수만큼 데이터 생성 후 append
+    data = np.unique(data, axis=0)  # 중복제거
 
 # 초기 레이블 생성 (불균형 데이터 생성)
-class_distribution = [0.5, 0.5]  # 클래스 0과 클래스 1의 비율
+class_distribution = [0.2, 0.8]  # 클래스 0과 클래스 1의 비율
 labels = np.random.choice([0, 1], size=(L, 1), p=class_distribution).astype(np.float32) # 0,1 중에 무작위 추출. 0: 0.5, 1: 0.5확률로 추출
 
-rand_test_data = np.random.randint(0, 200, (5, 2)).astype(np.float32)
+# 원 표시용 랜덤 테스트데이터 생성
+rand_test_data = np.random.randint(0, 200, (25, 2)).astype(np.float32)
 
 # test_data(2차원 그리드) 생성
 a = np.linspace(0, 199, 200)
@@ -58,11 +66,22 @@ def draw_image(k):
 
 
     ret, results, neighbor, dist = knn.findNearest(test_data, k=k)
-    results = results.reshape(x.shape)
+    color_results = results.reshape(x.shape)
 
     img = np.zeros((200, 200, 3), dtype=np.uint8)
     for label in range(2):
-        img[results == label] = back_colors[label]
+        img[color_results == label] = back_colors[label]
+    for i in range(test_data.shape[0]):
+        unique, cnt = np.unique(neighbor[i], return_counts=True)
+        max_cnt = np.max(cnt)
+        # if (cnt == max_cnt).sum() > 1 and (neighbor[i][0] != results.ravel()[i]):
+        # if (cnt == max_cnt).sum() > 1:
+            # radius = np.sqrt(dist[0, k - 1])
+            # # 빨간색에 가까우면 검정 파란색에 가까우면 흰색
+            # circle_color = 'b' if results[0,0] == 0 else 'r'
+            # circle = plt.Circle((test_data[i][0], test_data[i][1]), radius, color=circle_color, fill=False)
+            # plt.gca().add_patch(circle)
+            # print(f"Test Point: {test_data[i]}, Neighbors: {neighbor[i]}, Result: {results.ravel()[i]}")
 
 
     # k 값에 따라 이웃을 고려하여 원 그리기
@@ -72,9 +91,11 @@ def draw_image(k):
 
         ret, results, neighbor, dist = knn.findNearest(point.reshape(1, -1), k=k)
         unique, cnt = np.unique(neighbor, return_counts=True)
-        max_cnt = np.max(cnt)
 
-        if (cnt == max_cnt).sum() > 1 and (neighbor[0][0] != results):      # 동점인 경우 and (neighbor!=result)인 경우
+        max_cnt = np.max(cnt)
+        if (cnt == max_cnt).sum() > 1 and (neighbor[0][0] != results[0][0]):      # 동점인 경우 and (neighbor!=result)인 경우
+        # if (cnt == max_cnt).sum() > 1:
+        # if (neighbor[0][0] != results[0, 0]):
             radius = np.sqrt(dist[0, k - 1])
             # 빨간색에 가까우면 검정 파란색에 가까우면 흰색
             circle_color = 'b' if results[0][0] == 0 else 'r'
@@ -91,15 +112,18 @@ def draw_image(k):
 
     # 정확도 계산
     ret, result, neighbors, dist = knn.findNearest(data, k=k)
-    accuracy = np.mean(result == labels)
+    # accuracy = np.mean(result == labels)
+    accuracy = (np.squeeze(result) == np.squeeze(labels)).mean()*100
+
     print(f'Accuracy: {accuracy:#.2f}'+"%" " " f'k :  {k}')
 
 # 트랙바 생성
 cv2.namedWindow('KNN Visualization')
-cv2.createTrackbar('k', 'KNN Visualization', 1, 5, draw_image)
+cv2.createTrackbar('k', 'KNN Visualization', 1, 10, draw_image)
 
 # 초기 이미지 표시
 draw_image(1)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
